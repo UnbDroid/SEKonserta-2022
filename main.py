5fbe71f7893c9a3a058ffc5e33e02e12ffe77af1
Gustavo Joshua <87900265+joshuaazev@users.noreply.github.com>
2022-08-04 11:39:57 -0300

    Código Luigi
    Codigo inicial que contém as funções básicas da empilhadeira

---
+++ main.py
@@ -0,0 +1,65 @@
+#!/usr/bin/env python3
+from ev3dev2.motor import *
+from ev3dev2.sensor import *
+from ev3dev2.sensor.lego import *
+#from ev3dev.ev3 import *
+from ev3dev2.console import *
+from time import sleep
+import random
+import sys
+from ev3dev2.sound import Sound
+
+
+def sobe_empilhadeira():
+    MotorEmpilhadeira.on_for_seconds(SpeedPercent(-20), tempo_empilhadeira)
+    return
+
+def desce_empilhadeira():
+    MotorEmpilhadeira.on_for_seconds(SpeedPercent(20), tempo_empilhadeira)
+    return
+
+def abre_ultrassom():
+    MotorUltrassom.on_for_seconds(SpeedPercent(20), tempo_ultrassom)
+    return
+
+def fecha_ultrassom():
+    MotorUltrassom.on_for_seconds(SpeedPercent(-20), tempo_ultrassom)
+    return
+
+def pega_tubo():
+        abre_ultrassom()
+        tank_drive.on_for_seconds(SpeedPercent(10),SpeedPercent(10), 2.2)
+        sobe_empilhadeira()
+        time.sleep(3)
+        desce_empilhadeira()
+        tank_drive.on_for_seconds(SpeedPercent(-10),SpeedPercent(-10), 2.3)
+        fecha_ultrassom()
+        return
+
+# Módulos e suas portas -------------------------------------------------
+MotorEmpilhadeira = Motor(OUTPUT_A)
+MotorUltrassom = Motor(OUTPUT_B)
+tank_drive = MoveTank(OUTPUT_C, OUTPUT_D)  #C - Esquerda    D - Direita
+
+Ultrassom = UltrasonicSensor(INPUT_1)
+Giroscopio = GyroSensor(INPUT_2)
+coloresquerdo = ColorSensor(INPUT_3)
+colordireito = ColorSensor(INPUT_4)
+
+# Valores e Parâmetros ---------------------------------------------------
+
+tempo_empilhadeira = 2.5                    # Tempo necessário para subir/descer a empilhadeira
+tempo_ultrassom = 0.35                    # Tempo necessários para abrir/fechar o ultrassom
+
+
+
+
+# Início do código --------------------------------------------------------
+
+
+
+while True:
+    dist = Ultrassom.value()
+    print(dist, file=sys.stderr)
+    if dist < 67:
+        pega_tubo()
