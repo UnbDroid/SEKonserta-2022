dde8df272f8352087807378eed554049bfc4a855
Gustavo Joshua <87900265+joshuaazev@users.noreply.github.com>
2022-08-05 13:39:58 -0300

    Código Mario
    Conexão entre os dois cérebros, declaração dos sensores e atuadores, funções básicas da empillhadeira

--- main.py
+++ main.py
@@ -8,58 +8,54 @@ from time import sleep
 import random
 import sys
 from ev3dev2.sound import Sound
+import rpyc
 
+#bbk sempre à direita, e ele é conectado ao cabo
 
 def sobe_empilhadeira():
-    MotorEmpilhadeira.on_for_seconds(SpeedPercent(-20), tempo_empilhadeira)
+    MotorEmpilhadeira.on_for_seconds(SpeedPercent(15), tempo_empilhadeira)
     return
 
 def desce_empilhadeira():
-    MotorEmpilhadeira.on_for_seconds(SpeedPercent(20), tempo_empilhadeira)
+    MotorEmpilhadeira.on_for_seconds(SpeedPercent(-15), tempo_empilhadeira)
     return
 
-def abre_ultrassom():
-    MotorUltrassom.on_for_seconds(SpeedPercent(20), tempo_ultrassom)
-    return
-
-def fecha_ultrassom():
-    MotorUltrassom.on_for_seconds(SpeedPercent(-20), tempo_ultrassom)
-    return
-
-def pega_tubo():
-        abre_ultrassom()
-        tank_drive.on_for_seconds(SpeedPercent(10),SpeedPercent(10), 2.2)
-        sobe_empilhadeira()
-        time.sleep(3)
-        desce_empilhadeira()
-        tank_drive.on_for_seconds(SpeedPercent(-10),SpeedPercent(-10), 2.3)
-        fecha_ultrassom()
-        return
-
-# Módulos e suas portas -------------------------------------------------
-MotorEmpilhadeira = Motor(OUTPUT_A)
-MotorUltrassom = Motor(OUTPUT_B)
-tank_drive = MoveTank(OUTPUT_C, OUTPUT_D)  #C - Esquerda    D - Direita
-
-Ultrassom = UltrasonicSensor(INPUT_1)
-Giroscopio = GyroSensor(INPUT_2)
-coloresquerdo = ColorSensor(INPUT_3)
-colordireito = ColorSensor(INPUT_4)
-
-# Valores e Parâmetros ---------------------------------------------------
-
-tempo_empilhadeira = 2.5                    # Tempo necessário para subir/descer a empilhadeira
-tempo_ultrassom = 0.35                    # Tempo necessários para abrir/fechar o ultrassom
+conn = rpyc.classic.connect('192.168.0.1')
 
 
+ev3dev2_motor = conn.modules['ev3dev2.motor']
+ev3dev2_sensor = conn.modules['ev3dev2.sensor']
+ev3dev2_sensor_lego = conn.modules['ev3dev2.sensor.lego']
 
 
-# Início do código --------------------------------------------------------
+#ts = ev3dev2_sensor_lego.TouchSensor(ev3dev2_sensor.INPUT_1)
+SensorCorDireita = ColorSensor(INPUT_1)
+SensorCorEsquerda = ColorSensor(INPUT_2)
+Giroscopio = GyroSensor(INPUT_3)
+tank_drive = MoveTank(OUTPUT_D, OUTPUT_A)  # D - Esquerda  ------------- A - direita
+
+MotorEmpilhadeira = ev3dev2_motor.Motor(ev3dev2_motor.OUTPUT_A)
+UltrassomDireita = ev3dev2_sensor_lego.UltrasonicSensor(ev3dev2_sensor.INPUT_1)
+UltrassomEsquerda = ev3dev2_sensor_lego.UltrasonicSensor(ev3dev2_sensor.INPUT_2)
+LuzDireita = ev3dev2_sensor_lego.Sensor(ev3dev2_sensor.INPUT_3)
+LuzEsquerda = ev3dev2_sensor_lego.Sensor(ev3dev2_sensor.INPUT_4)
+
+# Variáveis e Parâmetros
+
+tempo_empilhadeira = 0.65
 
 
+# Início do Programa
+
+#desce_empilhadeira()
+MotorEmpilhadeira.on_for_seconds(SpeedPercent(15), 0.3)
 
 while True:
-    dist = Ultrassom.value()
-    print(dist, file=sys.stderr)
-    if dist < 67:
-        pega_tubo()
+    dist = UltrassomDireita.value()
+    if dist < 50:
+        tank_drive.on_for_seconds(SpeedPercent(10), SpeedPercent(10), 2)
+        sobe_empilhadeira()
+        time.sleep(1)
+        tank_drive.on_for_seconds(SpeedPercent(5), SpeedPercent(5), 2)
+        desce_empilhadeira()
+        tank_drive.on_for_seconds(SpeedPercent(-10), SpeedPercent(-10), 3)
