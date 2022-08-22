0510ab5f46390797d186657f15d5e81249b903b6
joshuaazev <gustavo.joshua81@gmail.com>
2022-08-22 10:56:40 -0300

    Commit Inicial

---
+++ Código Mario-Alpha/main.py
@@ -0,0 +1,55 @@
+#!/usr/bin/env pybricks-micropython
+from pybricks.hubs import EV3Brick
+from pybricks.ev3devices import (Motor, TouchSensor, ColorSensor,
+                                 InfraredSensor, UltrasonicSensor, GyroSensor)
+from pybricks.parameters import Port, Stop, Direction, Button, Color
+from pybricks.tools import wait, StopWatch, DataLog
+from pybricks.robotics import DriveBase
+from pybricks.media.ev3dev import SoundFile, ImageFile
+from pybricks.messaging import *
+import time
+from pybricks.nxtdevices import *
+
+# This program requires LEGO EV3 MicroPython v2.0 or higher.
+# Click "Open user guide" on the EV3 extension tab for more information.
+
+#Infos: Conectar o cérebro alpha por cabo no pc e o beta por bluetooth
+# Conectar eles por bluetooth pelo Alpha
+
+
+
+
+# Create your objects here.
+ev3 = EV3Brick()
+
+SensorCorDireita = ColorSensor(Port.S1)
+SensorCorEsquerda = ColorSensor(Port.S2)
+Giroscopio = GyroSensor(Port.S3)
+UltrassomFrente = UltrasonicSensor(Port.S4)
+RodaEsquerda = Motor(Port.D)
+RodaDireita = Motor(Port.A)
+robot = DriveBase(RodaEsquerda, RodaDireita, wheel_diameter=55.5, axle_track=104)
+
+# Write your program here.
+ev3.speaker.beep()
+
+server = BluetoothMailboxServer()
+mbox = TextMailbox('greeting', server)
+
+# The server must be started before the client!
+print('waiting for connection...')
+server.wait_for_connection()
+print('connected!')
+
+# In this program, the server waits for the client to send the first message
+# and then sends a reply.
+mbox.wait()
+print(mbox.read())
+mbox.send('hello to you!')
+
+while True:
+    print(mbox.read())
+    time.sleep(4)
+    if (mbox.read() == 'Oi sdds'):
+        ev3.speaker.beep()
+        #robot.turn(360)
---
+++ Código Mario-Beta/main.py
@@ -0,0 +1,65 @@
+#!/usr/bin/env pybricks-micropython
+from pybricks.hubs import EV3Brick
+from pybricks.ev3devices import *
+from pybricks.parameters import Port, Stop, Direction, Button, Color
+from pybricks.tools import wait, StopWatch, DataLog
+from pybricks.robotics import DriveBase
+from pybricks.media.ev3dev import SoundFile, ImageFile
+from pybricks.messaging import *
+from pybricks.nxtdevices import LightSensor
+import time
+
+
+
+def sobe_empilhadeira():
+    MotorEmpilhadeira.run_time(200, tempo_empilhadeira)
+    return
+
+def desce_empilhadeira():
+    MotorEmpilhadeira.run_time(-200, tempo_empilhadeira)
+    return
+
+
+
+
+# Motores e Sensores
+ev3 = EV3Brick()
+MotorEmpilhadeira = Motor(Port.A)
+UltrassomDireita = UltrasonicSensor(Port.S1)
+UltrassomEsquerda = UltrasonicSensor(Port.S2)
+LuzDireita = LightSensor(Port.S3)
+LuzEsquerda = LightSensor(Port.S4)
+
+# Variáveis
+tempo_empilhadeira = 850
+
+
+# Write your program here.
+ev3.speaker.beep()
+
+
+
+# This is the name of the remote EV3 or PC we are connecting to.
+SERVER = 'ev3dev'
+
+client = BluetoothMailboxClient()
+mbox = TextMailbox('greeting', client)
+
+print('establishing connection...')
+client.connect(SERVER)
+print('connected!')
+
+# In this program, the client sends the first message and then waits for the
+# server to reply.
+mbox.send('hello!')
+mbox.wait()
+print(mbox.read())
+mbox.send('Oie')
+
+while True:
+    time.sleep(2)
+    mbox.send('Oi sdds')
+    sobe_empilhadeira()
+    time.sleep(2)
+    desce_empilhadeira()
+    print("Sai")
