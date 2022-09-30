f531f9cfed3c2749a6bc6e0f7ef423b6920dd946
joshuaazev <gustavo.joshua81@gmail.com>
2022-09-30 09:01:43 -0300

    teste

--- CodigoMarioAlpha/main.py
+++ CodigoMarioAlpha/main.py
@@ -5,12 +5,72 @@ from pegatubo import *
 from servidor import*
 from percorregasoduto import *
 
-
+#sobe_empilhadeira(0.3)
+#desce_empilhadeira(0.1)
+#wait(3000)
+#sobe_empilhadeira()
+#wait(5000)
+#desce_empilhadeira()
+#abre_garra()
+#ev3.speaker.beep()
+#wait(3000)
+#fecha_garra()
+# while True:
+#     robot.drive(100,0)
+#     ValorCorEsquerda = SensorCorEsquerda.reflection()
+#     ValorCorDireita = SensorCorDireita.reflection()
+#     print("Valor na Esquerda é:", ValorCorEsquerda, "E na direita é", ValorCorDireita)
+#     if (ValorCorEsquerda < 15 or ValorCorDireita < 10):
+#         robot.stop()
+#sobe_empilhadeira(0.2)
 conecta_alpha_beta()
-sobe_empilhadeira()
-abre_garra()
+#conecta_alpha_luigi()
 
+# while True:
+#     ev3.speaker.say('CRISTIANOOOOOOOO')
+#     wait(1000)
+
+# while True:
+#     sobe_empilhadeira()
+#     wait(5000)
+#     desce_empilhadeira()
+#     wait(4000)
+
+#fecha_garra(0.2)
+#desce_empilhadeira()
+#sobe_empilhadeira(0.6)
+# while True:
+#     abre_garra()
+#     sobe_empilhadeira()
+#     fecha_garra()
+#     desce_empilhadeira()
+
+#conecta_alpha_beta()
+# sobe_empilhadeira()
+# desce_empilhadeira()
+# #fecha_garra()
+# abre_garra()
+# fecha_garra()
+
+#ev3.speaker.say('CRISTIANOOOOOOOO')
+
+#sobe_empilhadeira(0.5)
 percorre_gasoduto_esquerda()
 
-while True:
-    pega()
+# Giroscopio.reset_angle(0)
+# while True:
+#     fator = (0 - Giroscopio.angle())*3
+#     print(fator)
+#     robot.drive(220,fator)
+
+# sobe_empilhadeira(0.1)
+#while True:
+#    robot.drive(-100,0)
+#fecha_garra()
+#pega2(15)
+#pega2(15)
+#pega2(20)
+#sobe_empilhadeira(0.1)
+#pega2(15)
+#sobe_empilhadeira(0.1)
+#pega2(10)
